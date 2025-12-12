fd5e5b718e9d6b7ace439930768905e8b1381d52
Yehorchik69 <whowarner@gmail.com>
2025-12-12 23:35:24 +0200

    New task

---
+++ programB1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "    /\\_____/\\    \n";
+    std::cout << "   /  o  o  \\   \n";
+    std::cout << "  (     ^     )   \n";
+    std::cout << "   ============  \n";
+    std::cout << "   |  |  |  |    \n";
+    std::cout << "  /|  |  |  |\\   \n";
+    std::cout << " / |  |  |  | \\  \n";
+    std::cout << "(  |__|  |__|  ) \n";
+    std::cout << " \\            / \n";
+    std::cout << "  \\__________/  \n";
+
+    return 0;
+}
